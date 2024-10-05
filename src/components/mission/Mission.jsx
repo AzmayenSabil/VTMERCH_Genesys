f8d262c3e91ae36849e96b619d9b8952346f701f
Azmayen Fayek Sabil <77103994+AzmayenSabil@users.noreply.github.com>
2024-10-05 21:10:22 +0600

    Update Mission.jsx

--- src/components/mission/Mission.jsx
+++ src/components/mission/Mission.jsx
@@ -15,21 +15,23 @@ const Mission = () => {
           />
         </h3>
         <p
-          className="w-full font-montserrat text-[32px] sm:text-[36px] md:text-[42px] lg:text-[48px] font-bold 
-        leading-[40px] sm:leading-[45px] md:leading-[50px] lg:leading-[57.6px] tracking-[0.005em] text-left uppercase z-10 relative 
-        -mt-[7%] ml-[5%]"
+          className="w-full font-montserrat text-[18px] sm:text-[20px] md:text-[24px] lg:text-[30px] xl:text-[40px] 2xl:text-[48px] font-bold 
+  leading-[30px] sm:leading-[35px] md:leading-[40px] lg:leading-[40.6px] xl:leading-[50.6px] 2xl:leading-[57.6px] tracking-[0.005em] text-left uppercase z-10 relative 
+  -mt-[7%] ml-[5%] 
+  break-normal sm:break-words" // Allowing text to break normally
         >
-          Our mission is to enlighten, entertain <br />
-          and empower current and future <br />
+          Our mission is to enlighten, entertain{" "}
+          <br className="hidden md:block" />
+          and empower current and future <br className="hidden md:block" />
           leaders around the world.
         </p>
       </div>
 
-      <div className="absolute bottom-0 right-0 flex items-end justify-center z-0 w-1/2">
+      <div className="absolute bottom-0 right-0 flex items-end justify-center z-0 w-full md:w-1/2 mt-8 md:mt-0">
         <img
           src={actorImage}
           alt="Hero"
-          className="w-[571px] h-auto max-h-[calc(100%+200px)] object-contain sm:w-[400px] md:w-[500px] lg:w-[571px]" // Responsive width
+          className="w-[250px] sm:w-[250px] md:w-[400px] lg:w-[571px] h-auto max-h-[calc(100%+200px)] object-contain" // Responsive width
         />
       </div>
     </section>
